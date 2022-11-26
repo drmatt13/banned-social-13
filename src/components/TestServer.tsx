bd9e44e6fe0f901799c9996720749c26b5690347
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2022-11-26 16:45:17 -0500

    11/26 test

--- src/components/TestServer.tsx
+++ src/components/TestServer.tsx
@@ -1,21 +1,28 @@
-import serverService from "@/lib/serverService";
+// import serverService from "@/lib/serverService";
 
-async function getData() {
-  const data = await fetch(`${process.env.URL}/api/eventbus`, {
+import { cookies } from "next/headers";
+
+type Service = any;
+type Data = any;
+
+async function getData(service: Service, data?: Data) {
+  const nextCookies = cookies();
+  const res = await fetch(`${process.env.URL}/api/eventbus`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      Authorization: nextCookies.get("token")?.value
+        ? `Bearer ${nextCookies.get("token")?.value}`
+        : "",
     },
-    body: JSON.stringify({
-      service: "test",
-    }),
+    body: JSON.stringify({ service, ...data }),
   });
-  return data.json();
+  return res.json();
 }
 
 export default async function TestComponent() {
   // const data = await serverService("get user");
-  const { data } = await getData();
+  const { data } = await getData("test");
 
   return <div>data: {data}</div>;
 }
