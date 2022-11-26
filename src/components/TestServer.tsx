b34c5e5c5fef77cadc4c5819be82c281cf05ab91
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2022-11-26 16:36:42 -0500

    11/16

--- src/components/TestServer.tsx
+++ src/components/TestServer.tsx
@@ -1,15 +1,21 @@
 import serverService from "@/lib/serverService";
 
 async function getData() {
-  const data = await fetch("http://jsonplaceholder.typicode.com/todos/1");
+  const data = await fetch(`${process.env.URL}/api/eventbus`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      service: "test",
+    }),
+  });
   return data.json();
 }
 
 export default async function TestComponent() {
   // const data = await serverService("get user");
-  const data = await getData();
+  const { data } = await getData();
 
-  console.log(data);
-
-  return <div>user: test</div>;
+  return <div>data: {data}</div>;
 }
